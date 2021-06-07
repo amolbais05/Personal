3850e3a1bd82a75e49c53dcc9d9913e2aa3d0331
Amol Bais <amolsanjay.bais.ext@bpost.be>
2021-06-07 18:29:25 +0530

    Junit skeleton

---
+++ Junit/RealEstateApp/src/test/java/com/realestateapp/ApartmentRaterTest.java
@@ -0,0 +1,48 @@
+package com.realestateapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApartmentRaterTest {
+
+    @Test
+    void should_ReturnCorrectRating_When_CorrectApartment(){
+
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void should_ReturnErrorValue_When_IncorrectApartment(){
+
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void should_CalculateAverageRating_When_CorrectApartmentList(){
+
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void should_ThrowExceptionInCalculateAverageRating_When_EmptyApartmentList(){
+
+        // given
+
+        // when
+
+        // then
+    }
+}
---
+++ Junit/RealEstateApp/src/test/java/com/realestateapp/RandomApartmentGeneratorTest.java
@@ -0,0 +1,29 @@
+package com.realestateapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RandomApartmentGeneratorTest {
+
+    @Test
+    void should_GenerateCorrectApartment_When_DefaultMinAreaMinPrice(){
+
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void should_GenerateCorrectApartment_When_CustomMinAreaMinPrice(){
+
+        // given
+
+        // when
+
+        // then
+    }
+
+}
